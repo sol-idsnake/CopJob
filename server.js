dcaf9551b5bd1344664e69c1496acb6a2605eb69
Hendrik Wendt <h.wendt@gmx.com>
2018-06-23 18:36:04 -0400

    exporting app

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 'use strict'
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+};
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+};
 
 app.listen(process.env.PORT || 8080, () => console.log(
   `Your app is listening on port ${process.env.PORT || 8080}`));
+
+module.exports = { app, runServer, closeServer };
