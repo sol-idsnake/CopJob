a4b0350cbbeda0bdf55eeb38b73ca2843297804e
Hendrik Wendt <h.wendt@gmx.com>
2018-06-23 18:35:40 -0400

    added basic GET testing

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+chai.use(chaiHttp);
+
+const {app, runServer, closeServer} = require('../server')
+const expect = chai.expect
+
+describe('Root', function() {
+
+	it('should respond with a status code on GET', function() {
+		return chai.request(app)
+			.get('/')
+			.then(function(res) {
+				expect(res).to.have.status(200)
+			})
+	})
+})
