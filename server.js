c29d70c6293b6300826d74e6e6f62badd58f68ab
Hendrik Wendt <h.wendt@gmx.com>
2018-06-24 09:40:48 -0400

    updated

--- server.js
+++ server.js
@@ -1,44 +1,59 @@
 'use strict'
 
 const express = require('express');
-
 const app = express();
+const departmentRouter = require('./departmentRouter');
+const mongoose = require('mongoose');
+const {PORT, DATABASE_URL} = require('./config');
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-function runServer() {
-  const port = process.env.PORT || 8080;
+app.use("/", departmentRouter);
+
+app.use("*", function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app
-      .listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve(server);
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err)
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`)
+        resolve()
       })
-      .on("error", err => {
-        reject(err);
-      });
-  });
+        .on('error', err => {
+          mongoose.disconnect()
+          reject(err)
+        })
+    })
+  })
 };
 
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log("Closing server");
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);          
+        }
+        resolve();
+      });
     });
-  });
+  })
 };
 
-app.listen(process.env.PORT || 8080, () => console.log(
-  `Your app is listening on port ${process.env.PORT || 8080}`));
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
 
 module.exports = { app, runServer, closeServer };
