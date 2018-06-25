dd87f5371e130ca9494f67017ec4e4378f21e34e
sol-idsnake <h.wendt@gmx.com>
2018-06-25 13:15:25 -0400

    added POST method

--- departmentRouter.js
+++ departmentRouter.js
@@ -5,10 +5,6 @@ const {Department} = require('./models');
 const app = express();
 mongoose.Promise = global.Promise;
 
-// Use for POST method
-// const bodyParser = require('body-parser');
-// const jsonParser = bodyParser.json();
-
 app.use(express.json());
 
 router.get('/list', (req, res) => {
@@ -22,5 +18,33 @@ router.get('/list', (req, res) => {
 			res.status(500).json({ message: 'Internal server error' })
 		})
 });
+
+router.post('/create', (req, res) => {
+	const requiredFields = ["name", "link", "state", "salary", "description"];
+	for(let i=0; i < requiredFields.length; i++){
+		const field = requiredFields[i];
+		if(!(field in req.body)){
+			const message = `Missing \`${field}\` field in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		};
+	};
+
+	Department
+		.create({
+			name: req.body.name,
+			link: req.body.link,
+			state: req.body.state,
+			requirements: req.body.requirements,
+			salary: req.body.salary,
+			description: req.body.description
+		})
+		.then(post => res.status(201).json(post.serialize()))
+		.catch(err => {
+			console.error(err)
+			res.status(500).json({message: 'Internal server error'})
+		})
+		
+})
 
 module.exports = router;
