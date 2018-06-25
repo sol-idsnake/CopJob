3fc5f2d06bcd4cb2f8a979a59ac81fae3801fd84
sol-idsnake <h.wendt@gmx.com>
2018-06-25 13:51:15 -0400

    added PUT method

--- departmentRouter.js
+++ departmentRouter.js
@@ -43,8 +43,32 @@ router.post('/create', (req, res) => {
 		.catch(err => {
 			console.error(err)
 			res.status(500).json({message: 'Internal server error'})
-		})
-		
-})
+		});
+});
+
+router.put('/update/:id', (req, res) => {
+	if(!(req.params.id && req.body.id && (req.params.id === req.body.id))) {
+		const message = (
+			`Request path id (${req.params.id}) and request body id ` +
+	      `(${req.body.id}) must match`);
+	    console.error(message);
+	    return res.status(400).json({ message: message });
+	};
+
+	const toUpdate = {};
+	const updateableFields = ["name", "link", "state", "salary", "description"];
+
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			toUpdate[field] = req.body[field];
+		};
+	});
+
+	Department
+		.findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+		.then(post => res.status(204).end())
+		.catch(err => res.status(500).json({message: 'Internal server error'}))
+
+});
 
 module.exports = router;
