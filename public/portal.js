26ce8d08b74d4560d0b60e0e66a81f05820f909d
Hendrik Wendt <h.wendt@gmx.com>
2018-06-26 01:34:52 -0400

    adding form functionality

--- public/portal.js
+++ public/portal.js
@@ -5,14 +5,48 @@ $(formSubmitListener)
 function formSubmitListener() {
 
 	$('.js-form').submit(event => {
-		console.log('Form Submit')
 		event.preventDefault()
 
 		fetchInput()
+		// .then(validateInput(departmentObject))
 	})
 
 }
 
 function fetchInput() {
 	console.log('Fetching Input')
+
+	const position = $('#position').val()
+	const name = $('#name').val()
+	const link = $('#link').val()
+	const state = $('#state').val()
+	const age = $('#age').val()
+
+	let citizenship = 'yes'
+	if ($('#citizen-no').is(':checked')) {
+			citizenship = 'no'
+		}
+
+	let degree = 'High School degree required'
+	if ($('#associate').is(':checked')) {
+		degree = "Associate's degree required (2-years)"
+	} else if ($('#bachelor').is(':checked')) {
+		degree = 'Bachelor degree required (4-years)'
+	} else if ($('#masters').is(':checked')) {
+		degree = "Master's degree required"
+	} else if ($('#doctorate').is(':checked')) {
+		degree = "PhD required"
+	}
+
+	const salary = $('#salary').val()
+	const description = $('#description').val()
+
+	const departmentObject = [position, name, link, state, age, citizenship, degree, salary, description]
+	return departmentObject
+}
+
+function validateInput(departmentObject) {
+	console.log('Validating Input')
+
+	console.log(departmentObject)
 }
