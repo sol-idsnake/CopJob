f5a47f993ce8a92b3a63e7a8d678bacd980e7dd4
sol-idsnake <h.wendt@gmx.com>
2018-07-02 18:21:22 -0400

    added features & styling

--- public/index.js
+++ public/index.js
@@ -1,4 +1,9 @@
-$(showDatabase)
+$(displaySite)
+
+function displaySite() {
+	showDatabase()
+	deleteItem()
+}
 
 function showDatabase() {
 	const query = {
@@ -11,15 +16,61 @@ function showDatabase() {
 
 function renderDatabase(res) {
 	console.log(res)
+  res.forEach((item, index) => {
+    // ternary: if index divisible by 2, set className to gray
+    const className = index % 2 == 0 ? "gray" : "";
+    $(".list").append(
+      `<li class="${className}" id="${item.id}">
+      <span class="name">${item.name}</span>
+      <span class="state">${item.state}</span>
+      <span class="position">${item.position}</span>
+      <span class="link">${item.link}</span>
+      <i class="fas fa-trash-alt"></i>
+      <i class="fas fa-pencil-alt"></i>
+      </li>`
+    );
+  });
+}
 
-	res.forEach((item, index) => {
+function deleteItem() {
+	$('.list').on('click', '.fa-trash-alt', event => {
+		const itemId = $(event.currentTarget).parent().attr('id');
+		const itemName = $(event.currentTarget).siblings('span.name').text()
 
-		// ternary: if index divisible by 2, set className to gray
-		const className = index % 2 == 0 ? 'gray' : '';
-		$('.list').append(`<li class=${className}>${item.name}</li>`);
+		let confirmRemoval = false
+		confirmDelete(itemName)
+
+		if (confirmRemoval) {
+			const query = {
+				url: `/delete/${itemId}`,
+				method: "DELETE",
+				headers: {
+					id: `${itemId}`
+				},
+				success: renderConfirm
+			};
+			$.ajax(query);
+			reload()
+		}
 	})
+}
 
-	$('.sort').on('click', function(res, index) {
-		res.sort()
-	})
+function renderConfirm(itemId) {
+	$('.warningConfirm').append(`Successfully deleted item ID ${this.headers.id}`).delay(2000).fadeTo(800, 0)
+}
+
+function confirmDelete(itemName) {
+	$('.warningConfirm').append(`
+		<span class="confirmMessage">Remove item: ${itemName}?</span>
+		<div class="controls">
+      <button class="js-delete-confirm" value="js-form">Confirm</button>
+      <button class="js-delete-abort">Cancel</button>
+    </div>
+		`)
+	
+}
+
+function reload() {
+	$('.list').empty()
+	showDatabase()
 }
